53346eff9fecb40a0e589aef8e2709d23b373a23
Luiz Eduardo Barros Matos <f612247@corp.caixa.gov.br>
2022-03-29 20:51:43 -0300

    Adicionada página home

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 ## Ferramentas Utilizadas
 
+* NextJS
 * SASS
 
 ## Aprendizados
+
+### Next
+Next pode ser utilizado com Typescript.
 
 ### SASS
 Pré processador de css muito dinâmico, utilizando a biblioteca de sass, o processamento fica implícito.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,6 +9,21 @@ export default function Home() {
       <Head>
         <title>Início | Edu News</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Olá, bem-vindo</span>
+          <h1>Novidades sobre o universo <span>React</span></h1>
+          <p>
+            Tenha acesso a todas publicações <br />
+            <span>por apenas R$9,90 por mês</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img
+          src="/assets/avatar.svg"
+          alt="Garota de óculos programando em um notebook em cima de uma mesa com livros e café"
+          />
+      </main>
     </>
   )
 }
