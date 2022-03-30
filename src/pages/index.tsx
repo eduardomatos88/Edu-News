fa8dd810d2d8ea0ccd66884c76d794e4006d53ed
Luiz Eduardo Barros Matos <f612247@corp.caixa.gov.br>
2022-03-29 21:31:11 -0300

    Integrando Stripe ao projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { stripe } from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,9 +24,9 @@ export default function Home() {
           <h1>Novidades sobre o universo <span>React</span></h1>
           <p>
             Tenha acesso a todas publicações <br />
-            <span>por apenas R$9,90 por mês</span>
+            <span>por apenas {product.amount} por mês</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img
           src="/assets/avatar.svg"
@@ -26,4 +35,22 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KiotNAVtxKpobbb57k3MWML')
+
+  const product = {
+    priceId: price.id,
+    amount: Intl.NumberFormat('pr-br', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount / 100),
+
+  }
+  return {
+    props: {
+      product
+    }
+  }
 }
